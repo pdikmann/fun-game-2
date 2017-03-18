392d0b8f19033a939c24ff6c061776743666f522
Philipp Dikmann <mail@philippdikmann.de>
2017-03-18 03:12:52 +0100

    more progress with Box2Djs

--- src/sketch.js
+++ src/sketch.js
@@ -36,27 +36,85 @@ class Player {
   }
 }
 
+class Edge {
+  constructor( world, from, to ){
+    `world: Box2D.b2World,
+from: p5.Vector,
+to: p5.Vector`
+    this.from = from;
+    this.to = to;
+    let shape_floor = new Box2D.b2EdgeShape();
+    shape_floor.Set( new Box2D.b2Vec2( from.x, 
+                                       from.y ),
+                     new Box2D.b2Vec2( to.x,
+                                       to.y ))
+    let static_body_def = new Box2D.b2BodyDef();
+    let floor = world.CreateBody( static_body_def );
+    floor.CreateFixture( shape_floor, 0 );
+  }
+  draw(){
+    push();
+    stroke( 0, 255, 0 );
+    line( this.from.x,
+          this.from.y,
+          this.to.x,
+          this.to.y );
+    pop();
+  }
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 const p = new Player();
 const e = new Enemy();
+let edges = [];
 
 // create world
-let gravity = new Box2D.b2Vec2( 0, 10 );
+let gravity = new Box2D.b2Vec2( 0, 0 );
 let world = new Box2D.b2World( gravity );
 
-// create a floor
-let shape_floor = new Box2D.b2EdgeShape();
-shape_floor.Set( new Box2D.b2Vec2( 200, 300 ),
-                 new Box2D.b2Vec2( 300, 330 ))
-let static_body_def = new Box2D.b2BodyDef();
-let floor = world.CreateBody( static_body_def );
-floor.CreateFixture( shape_floor, 0 );
+// create four edges that contain the phyzax
+const corners = [ createVector( 150, 150 ),
+                  createVector( 350, 150 ),
+                  createVector( 350, 350 ),
+                  createVector( 150, 350 )];
+const pairs = [[0,1],[1,2],[2,3],[3,0]];
+for (i in pairs){
+  console.log( i );
+  edges.push( new Edge( world,
+                        corners[ pairs[i][0]],
+                        corners[ pairs[i][1]] ));
+}
+
+// create 2 more edges 
+edges.push(
+  new Edge( world,
+            createVector( 200, 300 ),
+            createVector( 250, 350 )));
+edges.push(
+  new Edge( world,
+            createVector( 350, 450 ),
+            createVector( 450, 400 )));
+
+// create a sphere
+let cfixshape = new Box2D.b2CircleShape();
+cfixshape.set_m_radius( 10 );
+let cfixdef = new Box2D.b2FixtureDef();
+cfixdef.set_shape( cfixshape );
+cfixdef.set_density( 1 );
+cfixdef.set_friction( 0 );
+cfixdef.set_restitution( 1 );
+let cboddef = new Box2D.b2BodyDef();
+cboddef.set_type( Box2D.b2_dynamicBody );
+cboddef.set_position( new Box2D.b2Vec2( 250, 220 ));
+let cbody = world.CreateBody( cboddef );
+cbody.CreateFixture( cfixdef );
 
 // create a box
 let shape = new Box2D.b2PolygonShape();
 shape.SetAsBox( 10, 10 );
 let fixture_def = new Box2D.b2FixtureDef();
 fixture_def.set_density( 10 );
+fixture_def.set_friction( 1 );
 fixture_def.set_restitution( 1 );
 fixture_def.set_shape( shape );
 let body_def = new Box2D.b2BodyDef();
@@ -64,9 +122,12 @@ body_def.set_type( Box2D.b2_dynamicBody );
 body_def.set_position( new Box2D.b2Vec2( 250, 250 ));
 let body = world.CreateBody( body_def );
 body.CreateFixture( fixture_def );
+body.SetLinearVelocity( new Box2D.b2Vec2( 0, 100 ));
 
 function setup() {
   createCanvas( 512, 512 );
+  ellipseMode( CENTER );
+  rectMode( CENTER );
 }
 
 function update(){
@@ -82,11 +143,23 @@ function draw() {
   //
   p.draw();
   e.draw();
+  // draw edges
+  for (i in edges){ edges[i].draw(); }
+  // draw box
   push();
   translate( body.GetPosition().get_x(),
              body.GetPosition().get_y() );
   rotate( body.GetAngle() );
-  rect( 0, 0, 10, 10 );
+  rect( 0, 0, 20, 20 );
+  line( 0, 0, 10, 0 );
+  pop();
+  // draw circle
+  push();
+  translate( cbody.GetPosition().get_x(),
+             cbody.GetPosition().get_y() );
+  rotate( cbody.GetAngle() );
+  ellipse( 0, 0, 20, 20 );
+  line( 0, 0, 10, 0 );
   pop();
   //
 }
